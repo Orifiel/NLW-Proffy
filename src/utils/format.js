1b5e621dc01e6a4116b96b9737aac16bb1b2c218
Cleiton <orifieleifiro@gmail.com>
2020-08-10 00:37:52 -0300

    fatoração: objetos e converter time

---
+++ src/utils/format.js
@@ -0,0 +1,40 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Ed. Física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+  ];
+  
+  const weekdays = [
+    "Domingo",
+    "Segunda-Feira",
+    "Terça-Feira",
+    "Quarta-Feira",
+    "Quinta-Feira",
+    "Sexta-Feira",
+    "Sabado",
+  ];
+  
+
+  function getSubject(subjectNumber) {
+    const position = +subjectNumber - 1
+    return subjects[position]
+  }
+
+  function convertHoursToMinutes(time) {
+      const { hour, minutes } =  time.split(':') //separa ex. 09:20 em hour = 09 e minutes = 20 
+      return Number((hour * 60) + minutes) //
+  }
+
+  module.exports = {
+      subjects,
+      weekdays,
+      getSubject,
+      convertHoursToMinutes
+  }
